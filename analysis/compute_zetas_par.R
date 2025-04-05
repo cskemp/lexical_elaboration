c6419e6c09e6ea9bd8f202723d29f13b800370cc
temuulenkh <temuulen.khishigsuren@student.unimelb.edu.au>
2025-04-05 12:52:37 +0800

    use original filtering for the main app

--- analysis/compute_zetas_par.R
+++ analysis/compute_zetas_par.R
@@ -12,12 +12,52 @@ source("bind_weights_par.R")
 
 d_wide <- read_csv(here("data", "foranalyses", "d_wide.csv"))
 
-# Compute L^lang score
-
 weights_lr <-  bind_weights_par(d_wide) %>%
   write_csv(here("output", "results", "hierarchical_lr_lang.csv"))
+
+my_mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+
+lang_data <- read_csv(here("data", "biladataset", "bila_dictionaries.csv")) %>%
+    select(-langname) %>%
+    # now use glottolog_langname
+    rename(langname = glottolog_langname) %>%
+    select(langname, glottocode, area, langfamily, longitude, latitude) %>%
+    unique() %>%
+    group_by(glottocode, area, langfamily, longitude, latitude) %>%
+    summarize(langname = my_mode(langname))
+
+app_data <- weights_lr %>%
+    select(lang, word, zeta) %>%
+    rename(glottocode = lang) %>%
+    left_join(lang_data, by = "glottocode") %>%
+    arrange(desc(zeta)) %>%
+    write_csv(here("output", "results", "bila_app_stats.csv"))
+
+# make smaller version for online Shiny app
+word_counts <- d_wide %>%
+    group_by(word) %>%
+    summarize(count = sum(count)) %>%
+    arrange(desc(count))
+
+top_words_medium <- word_counts %>%
+    head(6000)
+
+top_words_small <- top_words_medium %>%
+    head(2000)
+
+app_data_medium <- app_data %>%
+    filter(word %in% top_words_medium$word) %>%
+    write_csv(here("output", "results", "bila_app_stats_6000.csv"))
+
+app_data_small <- app_data %>%
+    filter(word %in% top_words_small$word) %>%
+    write_csv(here("output", "results", "bila_app_stats_2000.csv"))
 
 # Compute L^dict score
 
 weights_dict <-  bind_weights_dict(d_wide) %>%
   write_csv(here("output", "results", "hierarchical_lr_dict.csv"))
+
